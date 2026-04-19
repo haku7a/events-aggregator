7750f8a3549ad445bfa6dd2db58b144644f11260
haku7a <v.a.neyaskin@gmail.com>
2026-04-19 03:12:34 +0300

    style: export Base in app.db package

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,7 +1,9 @@
+from app.db.base import Base
 from app.db.session import AsyncSessionLocal, engine, get_session
 
 __all__ = [
     "get_session",
     "AsyncSessionLocal",
     "engine",
+    "Base",
 ]
